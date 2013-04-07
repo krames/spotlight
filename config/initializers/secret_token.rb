e887125ee36484d94bd01c42f8b2f6d77f1b5336
Kyle Rames <kyle@rames.org>
2013-04-07 15:13:38 -0500

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spotlight::Application.config.secret_token = '48cb6e5f9fec0ab59eb6052c89a81ecb04a29557cd9aac1c154dc419e445a5a3089108eddc4bb6d7f51ad62c1a72da71929d6c9503dbccdc35198cab527d6fd9'

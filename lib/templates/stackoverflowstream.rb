88b6dd506d3cc08251f25dc3959ece7ac0ea5e73
migu6418 <miguel.cantu@rackspace.com>
2013-09-06 15:14:26 -0500

    Added class to get stackoverflow mentions of rackspace

---
+++ lib/templates/stackoverflowstream.rb
@@ -0,0 +1,42 @@
+require 'serel' 
+
+
+Serel::Base.config(:stackoverflow, 's)td57wpsGmYg9MVp5apmw((') 
+
+puts Serel::Question.search().intitle("Rackspace").get()
+
+#question.intitle = "Rackspace"
+
+
+require 'serel'
+class StackOverFlowQuestions
+  
+  def initialize()
+    Serel::Base.config(:stackoverflow, 's)td57wpsGmYg9MVp5apmw((')
+    relation
+  end 
+  
+  def getRawSearchResults(word) 
+    questions = Serel::Question.search().intitle("#{word}")
+    relation = questions.get
+    return questions.get 
+  end
+  
+  def getUserDisplayName() 
+    relation.owner.display_name
+  end 
+  
+  def getTagArray() 
+    relation.tag
+  end 
+  
+  def getLink()
+    relation.link
+  
+  
+    
+    
+    
+end 
+    
+    
